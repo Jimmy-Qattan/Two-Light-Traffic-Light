922b6d6008f00ec9d5724a01badd63308718ded2
Jimmy-Qattan <59592522+Jimmy-Qattan@users.noreply.github.com>
2026-04-27 21:51:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -45,20 +45,97 @@ function circle(x, y, radius, color = "black") {
 }
 
 class LIGHT {
-  constructor(x, y, radius, color) {
+  constructor(x, y, radius, color, isRedLight) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     
     this.color = color;
+    this.nativeColor = color;
+    
+    this.initialized = false;
+    
     this.createLight();
+    this.initialize();
+    
+    this.isRedLight = isRedLight;
   }
   
   static backGroundColor = "white";
+  static allLights = [];
+  
+  static initialDel = 2000;
+  static durationVal = 5000;
+  static finalDel = 3000;
+  
+  static MIN = 100;
+  static MAX = 50000;
+  static DEFAULT = 2000;
+  
+  static validValueTime(val) {
+    if (val > this.MIN && val < this.MAX) {
+      return val;
+    } else {
+      if (Number.isFinite(val)) {
+        if (val > this.MAX) {
+          return this.MAX;
+        } else {
+          return this.MIN;
+        }
+      } else {
+        return this.DEFAULT
+      }
+      
+      return this.DEFAULT;
+    }
+  }
+  
+  static initButtons() {
+    const optionsDiv = document.querySelector("[lightOptions]")
+    const inputVals = optionsDiv.children;
+    const enterButton = optionsDiv.querySelector("button");
+
+    enterButton.addEventListener("click", () => {
+      this.initialDel = LIGHT.validValueTime(+inputVals[0].value);
+      inputVals[0].value = this.initialDel;
+      this.durationVal = LIGHT.validValueTime(+inputVals[1].value);
+      inputVals[1].value = this.durationVal;
+      this.finalDel = LIGHT.validValueTime(+inputVals[2].value)
+      inputVals[2].value = this.finalDel;
+   
+    })
+  }
+  
+  
+  
+  static running = false;
+  
+  static initializeSystemToStreetLightColors() {
+    this.allLights.forEach(light => {
+      if (light.isRedLight) {
+        light.setColor("black")
+      }
+    })
+  }
   
 
   createLight(colorVal = this.color, padding = 0) {
     circle(this.x ?? 0, this.y ?? 0, this.radius + padding ?? 20, colorVal ?? "yellow");
+  }
+  
+  initialize() {
+    if (!this.initialized) {
+      LIGHT.allLights.push(this);
+      this.initialized = true;
+    }
+    
+    return this;
+  }
+  
+  static allLightsOut() {
+    this.allLights.forEach(light => {
+      light.setColor("black")
+    })
   }
   
   static setBackGroundColor(color = "white") {
@@ -75,7 +152,7 @@ class LIGHT {
   replaceLightWithColor(colorVal = "red") {
     if (typeof colorVal != "string") return;
     this.removeLight();
-    this.setColor(colorVal);
+    this.color = colorVal;
     this.createLight();
   }
   
@@ -85,13 +162,13 @@ class LIGHT {
     this.replaceLightWithColor(colorVal);
   }
   
-  async transitionYellowToRed(duration = 1000) {
+  async transitionYellowToRed(duration = LIGHT.durationVal) {
     
-    if (Number.isFinite(duration) || duration < 0 || duration > 100000) return;
+    if (!Number.isFinite(duration) || duration < LIGHT.MIN || duration > LIGHT.MAX) return;
     
     let interpolation = 0;
     
-    return new Promise((resolve), function() {
+    return new Promise((resolve) => {
 
       const start = performance.now();
       let elapsed;
@@ -103,11 +180,11 @@ class LIGHT {
         const green = Math.floor(255 * (1 - interpolation));
         const currentColor = `rgb(255, ${green}, 0)`;
 
-        this.setColor(currentColor);
+        if (this.isRedLight) {
+          this.setColor(currentColor);
+        }
         
-        
-        
-        if (interpolation < 1) {
+        if (elapsed < duration) {
           requestAnimationFrame(tick);
         } else {
           resolve("Success");
@@ -117,17 +194,66 @@ class LIGHT {
     })
   }
   
+  static get systemReadyForStart() {
+    return (!this.running);
+  }
   
+  static resetSystem() {
+    this.allLights.forEach(light => {
+      light.setColor(light.isRedLight ? "black" : light.nativeColor);
+    })
+    // this.turnOffRedLights();
+    // this.redifyNonRedLights();
+  }
+  
+  static turnOffNonReds() {
+    this.allLights.forEach(light => {
+      if (!light.isRedLight) {
+        light.setColor("black");
+      }
+    })
+  }
+  
+  static async systemStart() {
+    if (!this.systemReadyForStart) return;
+    
+    const {promise, resolve} = Promise.withResolvers();
+    
+    window.setTimeout(() => {
+      resolve();
+    }, this.initialDel)
+    
+    await promise;
+    
+    this.running = true;
+    
+    this.resetSystem();
+    this.turnOffNonReds();
+  
+    for (let i = 0; i < this.allLights.length; i++) {
+        
+      const light = this.allLights[i].initialize();
+
+      if (light.isRedLight) {
+        await light.transitionYellowToRed(this.durationVal);
+      }
+    }
+    
+    window.setTimeout(() => {
+      this.resetSystem();
+      this.running = false;
+    }, this.finalDel)
+  }
 }
 
 LIGHT.setBackGroundColor("blue");
 rectangle(130, 120, 51, 90, "black");
-const myLight = new LIGHT(155, 100, 15, "red");
-const myLight2 = new LIGHT(155, 60, 15, "yellow");
+const myLight = new LIGHT(155, 100, 15, "green", false);
+const myLight2 = new LIGHT(155, 60, 15, "yellow", true);
 
-myLight2.transitionYellowToRed();
+LIGHT.initializeSystemToStreetLightColors();
+LIGHT.initButtons();
 
-
-
-// circle(155, 80, 15, "green");
-// circle(155, 40, 15, "red");
+document.querySelector("canvas").addEventListener("click", function() {
+  LIGHT.systemStart();
+})
