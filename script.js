1c1ab7f6f1ea89929d0c961e2f9f6060d718e967
Jimmy-Qattan <59592522+Jimmy-Qattan@users.noreply.github.com>
2026-04-27 18:16:51 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,98 @@
+const canvasEl = document.querySelector("canvas");
+const ctx = canvasEl.getContext('2d');
+
+
+
+function initializeCanvas(createBorder = true, color = "red") {
+  const canvas = document.createElement('canvas');
+  document.body.appendChild(canvas);
+  
+  canvas.style.width = "100%";
+  canvas.style.height = "100%";
+  
+  if (createBorder) {
+    canvas.style.border = `2px solid ${color}`;
+  }
+}
+
+function rectangle(x, y, elWidth, elHeight, color = "black", full = false) {
+  
+  if (typeof color != "string") return;
+  
+  const canvas = document.querySelector("canvas");
+  const canvasCtx = canvas.getContext('2d');
+  
+  if (full) {
+    canvasCtx.fillStyle = color;
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+    return;
+  }
+  
+  canvasCtx.fillStyle = color;
+  canvasCtx.fillRect(x, canvas.height - y, elWidth, elHeight);
+}
+
+function circle(x, y, radius, color = "black") {
+  if (typeof color != "string") return;
+  
+  const canvas = document.querySelector("canvas");
+  const canvasCtx = canvas.getContext('2d');
+  
+  canvasCtx.beginPath();
+  canvasCtx.arc(x, canvas.height - y, radius, 0, Math.PI * 2);
+  canvasCtx.fillStyle = color;
+  canvasCtx.fill();
+}
+
+class LIGHT {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    
+    this.color = color;
+    this.createLight();
+  }
+  
+  static backGroundColor = "white";
+  
+
+  createLight(colorVal = this.color, padding = 0) {
+    circle(this.x ?? 0, this.y ?? 0, this.radius + padding ?? 20, colorVal ?? "yellow");
+  }
+  
+  static setBackGroundColor(color = "white") {
+    if (typeof color != "string") return;
+    this.backGroundColor = color;
+    rectangle(0, 0, 0, 0, color, true);
+    
+  }
+  
+  removeLight() {
+    this.createLight(LIGHT.backGroundColor, 1);
+  }
+  
+  replaceLightWithColor(colorVal = "red") {
+    if (typeof colorVal != "string") return;
+    this.removeLight();
+    this.setColor(colorVal);
+    this.createLight();
+  }
+  
+  setColor(colorVal) {
+    if (typeof colorVal != "string") return;
+    this.color = colorVal;
+  }
+  
+  
+}
+
+LIGHT.setBackGroundColor("blue");
+rectangle(130, 120, 51, 90, "black");
+const myLight = new LIGHT(155, 100, 15, "red");
+const myLight2 = new LIGHT(155, 50, 15, "yellow");
+
+
+
+// circle(155, 80, 15, "green");
+// circle(155, 40, 15, "red");
